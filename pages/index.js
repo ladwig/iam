c2e06cfed786eac1995f6485c7f75c6d25ec46e8
Daniel Ladwig <ladwig.daniel@icloud.com>
2020-10-24 12:50:42 +0200

    Pile of shit

--- pages/index.js
+++ pages/index.js
@@ -9,23 +9,25 @@ export default function Start() {
         <title>Daniel Ladwig</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <section className={styles.picture}> 
+            <img src="/daniel.png" alt="my image" /> 
+          </section> 
       <main className={styles.main}>
         <section className={styles.hero}>
-          <h1 className={styles.welcome}>I'm <br></br>Daniel <br></br>Ladwig</h1>
-          <div className={styles.picture}> 
-            <img src="/daniel.png" alt="my image" /> 
-          </div> 
+          <h1 className={styles.welcome}>I'm Daniel Ladwig</h1>
           <div className={styles.buttongroup}>
             <Button type="main" url="mailto:ladwig.daniel@icloud.com" title="Contact me "name="contact"/>
             <Button type="secondary" url="google.de" title="Download CV" name="cv"/>
           </div>
         </section>
-      </main>
-
-      <footer className={styles.footer}>
-      
+        <footer className={styles.footer}>
+        <ul className={styles.menu}>
+          <li className={styles.item}><a href="#">Code</a></li>
+          <li className={styles.item}><a href="http//:linkedin.ladwig.digital" target="_blank">Experience</a></li>
+          <li className={styles.item}><a href="#">Music</a></li>
+        </ul>
       </footer>
+      </main>
     </div>
   )
 }
