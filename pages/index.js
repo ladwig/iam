255deafbe57c638c20b42f696fdfec2f8a501f04
Daniel Ladwig <ladwig.daniel@icloud.com>
2020-11-11 12:06:16 +0100

    Added simple meta description

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,15 @@ export default function Start() {
       <Head>
         <title>Daniel Ladwig</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Get to know Daniel Ladwig, student and digital native." />
       </Head>
       <main className={styles.main}>
     
           <h1 className={styles.welcome}>I'm Daniel Ladwig</h1>
         <ul className={styles.menu}>
-          <li className={styles.item}><a href="mailto:ladwig.daniel@icloud.com?subject=Hola!&body=How ya doin?">Contact</a></li>
-          <li className={styles.item}><a href="http://code.ladwig.digital" target="_blank">Code</a></li>
-          <li className={styles.item}><a href="http://linkedin.ladwig.digital" target="_blank">Experience</a></li>
+          <li className={styles.item}><a href="mailto:ladwig.daniel@icloud.com?subject=Hola!&body=Let me know something :)">Contact</a></li>
+          <li className={styles.item}><a href="http://code.ladwig.digital" rel="noreferrer"  target="_blank">Code</a></li>
+          <li className={styles.item}><a href="http://linkedin.ladwig.digital" rel="noreferrer" target="_blank">Experience</a></li>
     
         </ul>
 
