6c62cd380b5da742fa07dc46de93bdee62ced309
Daniel Ladwig <ladwig.daniel@icloud.com>
2021-04-09 18:00:42 +0200

    Tested some design ideas

---
+++ components/navigation.js
@@ -0,0 +1,7 @@
+import styles from '../styles/Navigation.module.css'
+
+export default function Navigation(props) {
+    return (
+        <div>Test</div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/main.module.css'
-
+import Navigation from "../components/navigation";
 export default function Start() {
   return (
     <div className={styles.container}>
@@ -9,12 +9,20 @@ export default function Start() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Get to know Daniel Ladwig, student and digital native." />
       </Head>
-      <main className={styles.background}>
 
+{/*      <div className={styles.background}>
           <div className={styles.dot + ' ' + styles.one}></div>
           <div className={styles.dot + ' ' + styles.two}></div>
           <div className={styles.dot + ' ' + styles.three}></div>
-      </main>
+      </div>*/}
+      <Navigation></Navigation>
+    <div className={styles.intro}>
+        <h1 className={styles.title}>Hello, <br></br> I'm Daniel Lad<span className={styles.thickLetter}>w</span>ig</h1>
+        <h4 className={styles.subtitle}>B.Sc. business informatics and passionate about the digital tomorrow </h4>
+    </div>
+        <div className={styles.contact}>
+
+        </div>
     </div>
   )
 }
