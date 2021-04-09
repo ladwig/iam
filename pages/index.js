1397afabde84427555b53af9c5fda7ca4af64591
Daniel Ladwig <ladwig.daniel@icloud.com>
2021-04-09 16:32:32 +0200

    Added animation for background

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import Head from 'next/head'
-import Button from '../components/button'
 import styles from '../styles/main.module.css'
 
 export default function Start() {
   return (
     <div className={styles.container}>
-
       <Head>
         <title>Daniel </title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Get to know Daniel Ladwig, student and digital native." />
       </Head>
-      <main className={styles.main}>
+      <main className={styles.background}>
 
-
-
+          <div className={styles.dot + ' ' + styles.one}></div>
+          <div className={styles.dot + ' ' + styles.two}></div>
+          <div className={styles.dot + ' ' + styles.three}></div>
       </main>
     </div>
   )
