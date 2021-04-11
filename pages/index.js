eebc02c0f6a5987e41abc312299529fd09716ba9
Daniel Ladwig <ladwig.daniel@icloud.com>
2021-04-11 21:31:03 +0200

    Added favicon

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,11 @@ export default function Start() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Daniel </title>
+        <title>Daniel Ladwig</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <meta name="description" content="Get to know Daniel Ladwig, student and digital native." />
       </Head>
 
