877d8600f88fada5c00f9944f53d56849f7ca6dc
Daniel Ladwig <ladwig.daniel@icloud.com>
2021-04-11 15:30:21 +0200

    Changed fonts

--- components/contact.js
+++ components/contact.js
@@ -73,7 +73,7 @@ export default function Contact() {
                             type="email"
                             onChange={handleOnChange}
                             required
-                            placeholder="john@doe.com"
+                            placeholder="your@mailadress.com"
                             value={inputs.email}
                         />
                         <textarea
--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Start() {
                             <>
                             <Navigation></Navigation>
                             <div className={styles.intro}>
-                                <h1>Hello, <br></br> I'm Daniel Lad<span className={styles.thickLetter}>w</span>ig</h1>
+                                <h1>Hello, <br></br>I'm Daniel Ladwig</h1>
                                 <h3>B.Sc. business informatics and passionate about the digital tomorrow </h3>
                             </div>
                             </>
