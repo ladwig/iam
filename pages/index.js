60ba5cb1d312ffa94720d7bcbec518c57fa590b5
Daniel Ladwig <ladwig.daniel@icloud.com>
2022-02-08 11:46:59 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Start() {
                             <Navigation></Navigation>
                             <div className={styles.intro}>
                                 <h1>Hello, <br></br>I'm Daniel Ladwig</h1>
-                                <h3 className={styles.description}>B.Sc. business informatics and passionate about the digital tomorrow </h3>
+                                <h3 className={styles.description}>Consultant for Technology Tranformation and passionate about the digital tomorrow </h3>
                             </div>
                                 <ArrowDown></ArrowDown>
                             </>
